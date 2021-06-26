a9006fff959a6d67f9d0e7c42c412f4a039c11cf
whitelotus718 <70615488+whitelotus718@users.noreply.github.com>
2021-06-26 17:29:03 -0400

    logo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Header from './components/Header'
 
 function App(props) {
   return (
     <div>
-      <h2>Hello World</h2>
+      <Header />
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
 function Header(props) {
+    const logo = <img src="https://www.gstatic.com/images/branding/product/1x/keep_2020q4_48dp.png" alt="logo" />
     return (
-        <div>
-
+        <div className="header">
+            {logo}
+            <h3>Keep</h3>
         </div>
     );
 }
