ff6c1b0a7b6a583935f12f648b25d37f548d79de
whitelotus718 <70615488+whitelotus718@users.noreply.github.com>
2021-06-26 18:38:46 -0400

    Add button created from raw CSS

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,15 +1,46 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function CreateArea (props) {
+    const [note, setNote] = useState({
+        title: "",
+        content: "",
+    });
+
+    function handleChange(e){
+        const {name, value} = e.target
+        setNote(preValue =>{
+            return {
+                ...preValue,
+                [name]: value
+            }
+        })
+    }
+
+    function handleSubmit(i){
+        i.prevent.default()
+        console.log(i);
+    }
+
+
     return (
         <div>
             <form>
-                <input type="text" placeholder="Title" name="title" ></input>
+                <input 
+                value={note.title}
+                type="text"
+                placeholder="Title"
+                name="title" 
+                onChange={handleChange}
+                />
                 <p>
-                    <textarea name="content" placeholder="Take a note...">
-                        {" "}
-                    </textarea>
+                    <textarea 
+                    value={note.content}
+                    name="content" 
+                    placeholder="Take a note..."
+                    onChange={handleChange}
+                    ></textarea>
                 </p>
+                <button onClick={handleSubmit}>Add</button>
             </form>
         </div>
     );
