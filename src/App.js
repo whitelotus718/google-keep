b9de712e18987355e27fb272a7b1d526e7c634d4
whitelotus718 <70615488+whitelotus718@users.noreply.github.com>
2021-06-26 17:49:29 -0400

    CreateArea componenet refers to the section where User will enter input in order to create a new Note

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './components/Header'
+import CreateArea from './components/CreateArea';
 
 function App(props) {
   return (
     <div>
       <Header />
+      <CreateArea />
     </div>
   );
 }
