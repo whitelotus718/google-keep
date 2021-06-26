87727a6f3c65ba486f7064c366855e2e2b7708a6
whitelotus718 <70615488+whitelotus718@users.noreply.github.com>
2021-06-26 18:11:23 -0400

    first note posted and styled

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './components/Header'
 import CreateArea from './components/CreateArea';
+import Note from './components/Note';
 
 function App(props) {
   return (
     <div>
       <Header />
       <CreateArea />
+      <Note />
     </div>
   );
 }
---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Note(props) {
+    return(
+        <div className="note">
+            <h1>My note</h1>
+            <p>Hey what's going on?</p>
+        </div>
+    );
+}
+
+export default Note;
